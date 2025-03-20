9681e725807fbbeb777dbf6ccc337e87612493cc
Merjen Rahmedova <merjenrahmedova@Merjens-MacBook-Pro.local>
2025-03-20 19:06:09 +0500

    Create API Request

--- RickAndMorty/APIClients/RMService.swift
+++ RickAndMorty/APIClients/RMService.swift
@@ -17,8 +17,13 @@ final class RMService{
     /// Send RIck and Morty API Call
     /// - Parameters:
     ///  - request: Request instance
+    ///  - type : The type of object we expected to get back
     ///  - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void){
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<T,Error>) -> Void
+    ){
         
     }
 }
